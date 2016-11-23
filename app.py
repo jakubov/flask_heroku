862db03edbff53859e7f3a3459241fc0467dd105
jakubov <robert.jakubov@gmail.com>
2016-11-22 23:50:38 -0500

    my hola mundo changes

--- app.py
+++ app.py
@@ -207,13 +207,13 @@ def get_temperature():
 def get_all_ip_addresses_app_usage():
     temperature_response = {}
     usage_list = []
+    usage_dict = {}
     for result in db.session.query(WeatherRequestsTracker).all():
         _res = result.__dict__
         ip_address = _res['ip_address']
         count = db.session.query(WeatherRequestsTracker).filter(
             WeatherRequestsTracker.ip_address == ip_address).count()
 
-        usage_dict = {}
         if ip_address not in usage_dict:
             usage_dict['ip_address'] = ip_address
             usage_dict['total'] = str(count)
